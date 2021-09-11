c298268326ee26805641743a3ad79028aca6f362
Cho Won Jae <yoteamo7@naver.com>
2021-09-11 18:07:36 +0900

    redis updated

--- server/utils/redis.js
+++ server/utils/redis.js
@@ -1,13 +1,10 @@
 import redis from "redis";
 
-const { NODE_ENV, REDIS_PORT_DEV, REDIS_PORT_PRO } = process.env;
+const { NODE_ENV, REDIS_PORT_DEV, REDIS_PORT_PRO, REDIS_HOST } = process.env;
 
 let redisClient;
 if (NODE_ENV === "production") {
-  redisClient = redis.createClient({
-    host: "redis-server",
-    port: REDIS_PORT_PRO,
-  });
+  redisClient = redis.createClient(REDIS_PORT_PRO, REDIS_HOST);
 } else {
   redisClient = redis.createClient(REDIS_PORT_DEV);
 }
