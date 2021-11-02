5934bfb2bc58036a557a7f0ab33c657ebf382911
Cho Won Jae <yoteamo7@naver.com>
2021-11-03 00:38:37 +0900

    log 추가

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -17,7 +17,7 @@ const userSchema = new mongoose.Schema(
     scrapShows: [{ type: mongoose.Types.ObjectId }],
     writeReviews: [{ type: mongoose.Types.ObjectId }],
 
-    appleId: Number,
+    appleId: String,
     kakaoId: Number,
     facebookId: Number,
     googleId: Number,
